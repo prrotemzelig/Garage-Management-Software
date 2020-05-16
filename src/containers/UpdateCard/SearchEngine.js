ac078d9affee4662fa67f7b8223d617907a0927d
arielas04 <50361739+arielas04@users.noreply.github.com>
2020-05-16 18:14:46 +0300

    change in car history page
    A change in the car history page, for each number we enter, a table with key details for the treatment is done, with a button showing all the details, when you click a button you will see a screen with all the details stored in the same car's Fairbase page as on the work card page

--- src/containers/UpdateCard/SearchEngine.js
+++ src/containers/UpdateCard/SearchEngine.js
@@ -1,109 +1,124 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-import classes from '../../components/Card/Card.module.css';
 import Card from '../../components/Card/CardSearch';
 import axios from '../../axios-cards';
 import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import * as actions from '../../store/actions/index';
+import ShowData from '../../components/Card/showData';
 import Spinner from '../../components/UI/Spinner/Spinner';
-import './Serch.css'
+
 class Search extends React.Component {
     constructor(props){
         super(props);
         this.state = {
+          branchNumber:'',
           carNumber:'',
           found: false,
-          data: [],
+          click: false
         }
+        this.onChildClicked = this.onChildClicked.bind(this);
+
       }
   
-    componentDidMount() { // we want to fetch all the cards. so for doing that, I need to implement componentDidMount
-      this.props.onFetchCards(this.props.token, this.props.userId);
+  componentDidMount() { // we want to fetch all the cards. so for doing that, I need to implement componentDidMount
+      this.props.onFetchCards(this.props.token, this.props.userId,this.props.branchNumber);
       
+  }
+  onChildClicked() {
+    this.setState({click : true });
   }
   check(data){
     if(data.cardData.licenseNumber===this.state.carNumber){
       this.state.found=true;
-      //this.state.data.push(data);
-      //this.state.dataBaseCarNumber=data.cardData.licenseNumber;
-      //this.state.carDetails=data.carData;
-      //this.state.cardDetails=data.cardData;
-      //this.state.customer_details=data.customerData;
-      //console.log(this.state.carDetails);
-      //console.log(this.state.cardDetails);
-      //console.log(this.state.customer_details); 
     }
   }
   
-  
     render () {
+      var f;
       let cards;
-      console.log(this.props.value);
       this.state.carNumber= this.props.value;
+      console.log(this.state.click);
           if(this.state.carNumber!== "" ){
             cards = <Spinner />;
             if ( !this.props.loading ) { // if it not true - if we not loading
               cards = this.props.cards.map( card => (
-                this.check(card),  
-              <Card
+                this.check(card),
+                <Card
                   data={this.state.carNumber}
                   key={card.id}
-                  cardData={card.cardData}/> 
+                  customerData={card.customerData}
+                  carData={card.carData}
+                  cardData={card.cardData}
+                  onClicked={this.onChildClicked}/> 
                ))
-               //this.check(card)
             }
           }
-      //if(this.state.found && this.state.carNumber!==''){
+          console.log(cards);
       if(!this.state.found && this.state.carNumber!==''){
         return ( 
           <div >
-            <h5>לא נמצאה היסטוריה עבור רכב זה </h5></div>
-         
-      );
-      }else{
-      return (
-          <div >
-              {cards} 
-          </div>
+            <h5>לא נמצאה היסטוריה עבור רכב זה </h5></div> 
       );
       }
-      /*}if(!this.state.found && this.state.carNumber!==''){
-        return (
-          
-          <div >
-            <div className={classes.Card} >
-              <div className="result">
-              <h5>לא נמצאה היסטוריה לרכב זה</h5>
-              </div>
-            </div>
-          </div>
-      );
+      if(this.state.found && this.state.carNumber!=='' && !this.state.click){
+        
+      return (
+        <table class="table " style={{direction: "rtl",fontFamily: "Alef Hebrew"}}>
+                <thead>
+                    <tr style={{fontWeight: "bold", fontSize: "18px"}}>
+                        <td scope="col" >שם לקוח</td>
+                        <td scope="col" >מספר רכב</td>
+                        <td scope="col" >תאריך הנזק</td>
+                        <td scope="col" >מספר כרטיס</td>
+                        <td scope="col" >תאריך פתיחה</td> 
+                         
+                    </tr>
+                    
+                </thead>
 
+                <tbody >
+                    {cards}
+  
+                </tbody>
+                
+            </table>        
+      );
+      }
+      if(this.state.found && this.state.click){
+        console.log("aa");
+        return(
+          <div>  
+              <ShowData
+                value={this.state.carNumber}
+                />
+            </div> 
+        );     
       }
       else{
         return(
           <div></div>
         );
-      }*/
+        
+      }
     }
-  
 }
 const mapStateToProps = state => { // here we get the state and return a javascript object
   return {
       cards: state.card.cards, // we get my cards from state. we state cards we are reaching out to the card reducer and with cards we then reach out to cards property in the state of my reducer 
       loading: state.card.loading,
       token: state.auth.token,
-      userId: state.auth.userId
+      userId: state.auth.userId,
+      branchNumber: state.auth.branchNumber
   };
 };
 
 const mapDispatchToProps = dispatch => { // for this to work we need to connect this constant "mapDispatchToProps" with our component 
   return {
-      onFetchCards: (token,userId) => dispatch( actions.fetchCards(token, userId) ) //  return a map to map my props to dispatchable functions
+      onFetchCards: (token,userId,branchNumber) => dispatch( actions.fetchCards(token, userId,branchNumber) ),
+      //  return a map to map my props to dispatchable functions
       //here we want to execute an anonymous function where we eventually dispatch the action we just created it
       // note - we need to execute this function - "fetchCards()" to really get the action
   };
 };
 
-export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( Search, axios ) ); 
-
+export default connect( mapStateToProps, mapDispatchToProps )( withErrorHandler( Search, axios ) );
