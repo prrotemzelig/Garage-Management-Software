aeb2c8339674a710c777d9e022340c734b715f50
arielas04 <50361739+arielas04@users.noreply.github.com>
2020-06-01 18:57:59 +0300

    mail sending from web

--- src/containers/DailyReports/DatePic.js
+++ src/containers/DailyReports/DatePic.js
@@ -3,7 +3,6 @@ import DatePicker from 'react-datepicker';
 import './style.css'
 import "react-datepicker/dist/react-datepicker.css";
 //import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'react-bootstrap';
 
 class Datepic extends React.Component {
 
